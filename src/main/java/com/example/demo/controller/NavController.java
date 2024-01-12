f00392a34189cb7e805cc621306c00a269b47bff
Tanu C <Tanu C@LAPTOP-7S49GQ3Q>
2024-01-12 15:38:20 +0530

    First version of tunehub

---
+++ src/main/java/com/example/demo/controller/NavController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@Controller
+public class NavController {
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+		}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+	
+	@GetMapping("/newSong")
+	public String newSong() {
+		return "newSong";
+	}
+	
+
+}
